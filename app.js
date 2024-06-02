0c66bcd72c2bed1a943b3201db6ea0ffa33a5b15
anmol <108050140+mranmolv1@users.noreply.github.com>
2024-06-02 11:17:09 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -18,9 +18,28 @@ leftBtn.addEventListener('click',()=>{
     if(slideNumber > 1) {
     slide.style.transform = `translateX(${-(slideNumber-2)*100}%)`;
     slideNumber--;
-    console.log(slideNumber);
     }else{
         slide.style.transform = `translateX(-${(img.length-1)*100}%)`
         slideNumber = img.length;
     }
 });
+
+// script for theme change
+
+const currentTab = document.querySelector(".selected-tab");
+console.log(currentTab.textContent); 
+
+if (currentTab.textContent.match("Illustrations")) {
+    document.body.classList.toggle("green-theme");
+}
+if (currentTab.textContent.match("Icons")) {
+    document.body.classList.toggle("red-theme");
+}
+if (currentTab.textContent.match("Cartoon Stripes")) {
+    document.body.classList.toggle("yellow-theme");
+}
+if (currentTab.textContent.match("Designs")) {
+    document.body.classList.toggle("blue-theme");
+}
+
+// end
